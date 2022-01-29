e6229c85b180d0b9410254ebfbcc367355371956
ezome <stayle8186@gmail.com>
2022-01-29 18:55:10 +0300

    less2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import styler from "./index.module.css";
 
-function Message({ text }) {
-  return <p>{text}</p>;
-}
-
 const App = () => {
-  const text = "LALALA";
+  const [messageList, setMessageList] = useState([]);
+  const [value, setValue] = useState("");
+  const handleChange = (e) => setValue(e.target.value);
+
+  useEffect(() => {
+    if (
+      messageList.length &&
+      messageList[messageList.length - 1].author !== "Bot"
+    ) {
+      setMessageList([...messageList, { author: "Bot", text: "Test" }]);
+    }
+  }, [messageList]);
+
+  const addMessageList = (e) => {
+    setMessageList([...messageList, { author: "User", text: value }]);
+    setValue("");
+    e.preventDefault();
+  };
 
   return (
-    <div className={styler.box}>
-      <h1>AlALAL</h1>
-      <Message text={text} />
+    <div>
+      {messageList.map((message) => (
+        <p>
+          <b>{message.author}</b>: {message.text}
+        </p>
+      ))}
+      <form onSubmit={addMessageList}>
+        <input type="text" value={value} onChange={handleChange} />
+        <input type="submit" value="Отправить" />
+      </form>
     </div>
   );
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
